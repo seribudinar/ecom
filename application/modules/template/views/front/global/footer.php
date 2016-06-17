d5883937e9692dcd8b7bc4193c12426da884fbdd
maful <mafulprayoga@gmail.com>
2016-06-17 07:26:43 +0700

    Added copyright on Footer

--- application/modules/template/views/front/global/footer.php
+++ application/modules/template/views/front/global/footer.php
@@ -1,3 +1,12 @@
+<div class="ui black inverted vertical footer segment">
+  <div class="ui center aligned container">
+    <div class="ui inverted section divider"></div>
+    <div class="ui horizontal inverted small divided link list">
+      <a class="item" href="http://mafulprayogaarnandi.blogspot.com/" target="_blank">&copy; Maful Prayoga Arnandi <?php echo date("Y");?></a>
+    </div>
+  </div>
+</div>
+
 <script src="<?php echo base_url();?>assets/sm/jquery.min.js"></script>
 <script src="<?php echo base_url();?>assets/sm/semantic.min.js"></script>
 <script src="<?php echo base_url();?>assets/sm/main.js"></script>
